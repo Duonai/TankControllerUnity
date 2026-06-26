001a30cd299893e3e673f24c77c6f22da52fac4b
Duonai <rwby2373@gmail.com>
2026-06-26 16:48:11 +0900

    적군 AI 구현 및 LLM output 입력 통신 확인 완료

---
+++ Assets/Scripts/Fire_Control_Scripts/Fire_Control_Input_01_Desktop_CS.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+
+namespace ChobiAssets.KTP
+{
+
+    public class Fire_Control_Input_01_Desktop_CS : Fire_Control_Input_00_Base_CS
+    {
+#if !UNITY_ANDROID && !UNITY_IPHONE
+
+        private Socket_Communicator2_CS socket;
+
+        private void Start()
+        {
+            socket = GameObject.Find("Communicator2").GetComponent<Socket_Communicator2_CS>();
+        }
+
+        public override void Get_Input()
+        {
+            //if (Key_Bindings_CS.IsFireKeyPressing())
+            //{
+            //    fireControlScript.Fire();
+            //}
+
+            string fire = socket.fire;
+
+            //Debug.Log("Fire Input: " + fire);
+
+            if (fire != "idle")
+            {
+                fireControlScript.Fire();
+            }
+        }
+#endif
+    }
+
+}
